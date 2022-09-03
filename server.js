c2303bc2926c30da8d443b6a46072ff5e52d19b4
Haley Seymour <haleycseymour@comcast.net>
2022-09-03 09:56:15 -0700

    basic back end structure plus/minus specific auth features

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Requiring our models for syncing
+const { User, Blog } = require('./models');
+
+// Sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
